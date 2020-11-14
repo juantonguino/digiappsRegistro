7ddb34b08aecbd8971496e0ceea9927631abddbc
juantonguino <juan.tonguino@gmail.com>
2020-11-14 15:53:02 -0500

    redirect modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 
-  redirectSite: "http://ec2-3-85-9-193.compute-1.amazonaws.com/gracias-por-registrarte/"
+  redirectSite: "http://ec2-54-81-245-250.compute-1.amazonaws.com/gracias-por-registrarte/"
 };
